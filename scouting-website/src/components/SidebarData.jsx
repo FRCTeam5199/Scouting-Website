dbfbfd4651e4529d64836cc1dd85f5198736894c
RishabhIsAwesome <rishkprabhat@gmail.com>
2026-03-06 07:21:24 -0800

    Commented out unused pages

--- scouting-website/src/components/SidebarData.jsx
+++ scouting-website/src/components/SidebarData.jsx
@@ -17,22 +17,22 @@ export const SidebarData = [
     icon: <IoIcons.IoIosPaper />,
     cName: 'nav-text'
   },
-  {
-    title: 'Pit Scouting',
-    path: '/pit-scouting',
-    icon: <FaIcons.FaCartPlus />,
-    cName: 'nav-text'
-  },
-  {
-    title: 'Review',
-    path: '/review',
-    icon: <IoIcons.IoMdPeople />,
-    cName: 'nav-text'
-  },
-  {
-    title: 'Settings',
-    path: '/settings',
-    icon: <FaIcons.FaCog />,
-    cName: 'nav-text'
-  },
+  // {
+  //   title: 'Pit Scouting',
+  //   path: '/pit-scouting',
+  //   icon: <FaIcons.FaCartPlus />,
+  //   cName: 'nav-text'
+  // },
+  // {
+  //   title: 'Review',
+  //   path: '/review',
+  //   icon: <IoIcons.IoMdPeople />,
+  //   cName: 'nav-text'
+  // },
+  // {
+  //   title: 'Settings',
+  //   path: '/settings',
+  //   icon: <FaIcons.FaCog />,
+  //   cName: 'nav-text'
+  // },
 ];
